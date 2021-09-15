a8f208095e0276100d449559d4e89e14d3717709
haodong <1193299044@qq.com>
2021-09-15 18:52:29 +0800

    feat:增加三种类型繁体字转化成简体字功能

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from WordsSearch import IndexSearch
+from words_search import IndexSearch
+from translate import Translate
 
 if __name__ == "__main__":
     s = "法轮功|flg|法lun功"
     test = "法轮功是一种邪教组织，简称flg，抵制法lun功"
 
     search = IndexSearch()
+    translate = Translate()
+
+    test = translate.ToSimplifiedChinese(test)
+    s = translate.ToSimplifiedChinese(s)
+
     search.SetKeywords(s.split('|'))
     print("搜索信息：", search.FindAll(test))
     print("关键词替换：", search.Replace(test))
